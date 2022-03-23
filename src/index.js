a1dc3f3473b261d9118ee0081ec9ef416d7ea0f2
clereeze <88924520+clereeze@users.noreply.github.com>
2022-03-23 16:58:22 +0800

    Updated HTML, CSS, Javascript folders with latest code

---
+++ src/index.js
@@ -0,0 +1,69 @@
+//day and time 
+let now = new Date();
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[now.getDay()];
+
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+};
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes =  `0${minutes}`;
+};
+
+document.querySelector("#day-time").innerHTML = `${day}, ${hour}:${minutes}`;
+
+//temperature and city
+
+function displayWeatherCondition (response) {
+document.querySelector("#city-name").innerHTML = response.data.name;
+document.querySelector("#weather").innerHTML = response.data.weather[0].description;
+document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+};
+
+function search(city) {
+  let apiKey = "7b164cdced7aaeb17590e6fb8707df24";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-bar").value;
+  search(city);
+};
+
+document.querySelector("#search-button").addEventListener("click", handleSubmit);
+search("Singapore"); 
+
+function searchLocation(position) {
+let apiKey = "7b164cdced7aaeb17590e6fb8707df24";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${apiKey}&units=metric`;
+let longitude = position.coords.longitude; 
+let latitude = position.coords.altitude; 
+let currentCity = "Singapore";
+document.querySelector("#city-name").innerHTML = `You are currently in ${currentCity}, with coordinates ${longitude}, ${latitude}`;
+axios.get(`${apiUrl}`).then(displayWeatherCondition);
+};
+
+function updateCurrent(event) {
+event.preventDefault;
+navigator.geolocation.getCurrentPosition(searchLocation);
+};
+
+let currentTemp = document.querySelector("#current-button"); 
+currentTemp.addEventListener("click", updateCurrent);
+
+
+
+
+
+
+
+
+
+
+
+
